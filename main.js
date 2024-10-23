7acacd1220abdce9bb8c99b3d586a66a8f1eb7b4
NataliiaRosol <93214918+NataliiaRosol@users.noreply.github.com>
2024-10-23 17:24:44 +0300

    finished mobile version

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+const menuBtn = document.querySelector('.menu-icon');
+const menuClose = document.querySelector('.menu-close');
+const menuList = document.querySelector('.menu-list');
 
+menuBtn.addEventListener('click', ()=> {
+  menuList.classList.toggle('open');
+});
+
+menuClose.addEventListener('click', ()=> {
+  menuList.classList.remove('open');
+});
 
 
 const swiper = new Swiper('.swiper', {
@@ -9,6 +19,31 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    780:{
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1200:{
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1625: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
   },
 
   
